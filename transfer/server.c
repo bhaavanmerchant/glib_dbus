5a60cad303bc0c48670e44e3426597a5309541b8
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2013-04-28 12:50:50 +0530

    Added a XML client server working over dbus

---
+++ transfer/server.c
@@ -0,0 +1,172 @@
+#include <gio/gio.h>
+#include <stdlib.h>
+
+#ifdef G_OS_UNIX
+#include <gio/gunixfdlist.h>
+/* For STDOUT_FILENO */
+#include <unistd.h>
+#endif
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+static GDBusNodeInfo *introspection_data = NULL;
+
+/* Introspection data for the service we are exporting */
+static const gchar introspection_xml[] =
+  "<node>"
+  "  <interface name='org.gnome.Transfers'>"
+  "    <method name='TransferQueue'/>"
+  "  </interface>"
+  "</node>";
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+static void
+handle_method_call (GDBusConnection       *connection,
+                    const gchar           *sender,
+                    const gchar           *object_path,
+                    const gchar           *interface_name,
+                    const gchar           *method_name,
+                    GVariant              *parameters,
+                    GDBusMethodInvocation *invocation,
+                    gpointer               user_data)
+{
+    if (g_strcmp0 (method_name, "TransferQueue") == 0)
+    {
+
+      if (g_dbus_connection_get_capabilities (connection) & G_DBUS_CAPABILITY_FLAGS_UNIX_FD_PASSING)
+        {
+          GDBusMessage *reply;
+          GUnixFDList *fd_list;
+          GError *error;
+
+          fd_list = g_unix_fd_list_new ();
+          error = NULL;
+          g_unix_fd_list_append (fd_list, STDOUT_FILENO, &error);
+          g_assert_no_error (error);
+
+          reply = g_dbus_message_new_method_reply (g_dbus_method_invocation_get_message (invocation));
+          g_dbus_message_set_unix_fd_list (reply, fd_list);
+
+          error = NULL;
+          g_dbus_connection_send_message (connection,
+                                          reply,
+                                          G_DBUS_SEND_MESSAGE_FLAGS_NONE,
+                                          NULL, /* out_serial */
+                                          &error);
+          g_assert_no_error (error);
+
+          g_object_unref (invocation);
+          g_object_unref (fd_list);
+          g_object_unref (reply);
+        }
+      else
+        {
+          g_dbus_method_invocation_return_dbus_error (invocation,
+                                                      "org.gtk.GDBus.Failed",
+                                                      "Your message bus daemon does not support file descriptor passing (need D-Bus >= 1.3.0)");
+        }
+
+    }
+}
+
+static gchar *_global_title = NULL;
+
+static gboolean swap_a_and_b = FALSE;
+
+static GVariant *
+handle_get_property (GDBusConnection  *connection,
+                     const gchar      *sender,
+                     const gchar      *object_path,
+                     const gchar      *interface_name,
+                     const gchar      *property_name,
+                     GError          **error,
+                     gpointer          user_data)
+{
+  return NULL;
+}
+
+static gboolean
+handle_set_property (GDBusConnection  *connection,
+                     const gchar      *sender,
+                     const gchar      *object_path,
+                     const gchar      *interface_name,
+                     const gchar      *property_name,
+                     GVariant         *value,
+                     GError          **error,
+                     gpointer          user_data)
+{
+  return TRUE;
+}
+
+
+/* for now */
+static const GDBusInterfaceVTable interface_vtable =
+{
+  handle_method_call,
+  handle_get_property,
+  handle_set_property
+};
+
+static void
+on_bus_acquired (GDBusConnection *connection,
+                 const gchar     *name,
+                 gpointer         user_data)
+{
+  guint registration_id;
+
+  registration_id = g_dbus_connection_register_object (connection,
+                                                       "/org/gnome/Transfers",
+                                                       introspection_data->interfaces[0],
+                                                       &interface_vtable,
+                                                       NULL,  /* user_data */
+                                                       NULL,  /* user_data_free_func */
+                                                       NULL); /* GError** */
+  g_assert (registration_id > 0);
+
+}
+
+static void
+on_name_acquired (GDBusConnection *connection,
+                  const gchar     *name,
+                  gpointer         user_data)
+{
+}
+
+static void
+on_name_lost (GDBusConnection *connection,
+              const gchar     *name,
+              gpointer         user_data)
+{
+  exit (1);
+}
+
+int
+main (int argc, char *argv[])
+{
+  guint owner_id;
+  GMainLoop *loop;
+
+  g_type_init ();
+
+  introspection_data = g_dbus_node_info_new_for_xml (introspection_xml, NULL);
+  g_assert (introspection_data != NULL);
+
+  owner_id = g_bus_own_name (G_BUS_TYPE_SESSION,
+                             "org.gnome.TransfersBus",
+                             G_BUS_NAME_OWNER_FLAGS_NONE,
+                             on_bus_acquired,
+                             on_name_acquired,
+                             on_name_lost,
+                             NULL,
+                             NULL);
+
+  loop = g_main_loop_new (NULL, FALSE);
+  g_main_loop_run (loop);
+
+  g_bus_unown_name (owner_id);
+
+  g_dbus_node_info_unref (introspection_data);
+
+  return 0;
+}
